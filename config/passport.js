f21cea1a2d0e91aed84573cc2d2f0be801278c87
MajidHussainMK <majid.hussain.mk@gmail.com>
2019-03-05 17:01:18 +0500

    final

--- config/passport.js
+++ config/passport.js
@@ -4,14 +4,9 @@ const mongoose = require("mongoose");
 const User = mongoose.model("users");
 const keys = require("../config/keys");
 
-require("dotenv").config();
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = keys.secretOrKey;
-
-if (opts.secretOrKey === null) {
-  opts.secretOrKey = process.env.SECRET;
-}
 
 module.exports = passport => {
   passport.use(

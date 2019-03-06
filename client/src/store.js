d6355611d47ee6bb228f34327f5b42ba52939b76
MajidHussainMK <majid.hussain.mk@gmail.com>
2019-03-06 15:52:42 +0500

    Ready to go

--- client/src/store.js
+++ client/src/store.js
@@ -1,41 +1,41 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers";
-
-const initialState = {};
-
-const middleware = [thunk];
-
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
-
-export default store;
 // import { createStore, applyMiddleware, compose } from "redux";
 // import thunk from "redux-thunk";
-// import rootReducer from "./reducers/index";
+// import rootReducer from "./reducers";
 
 // const initialState = {};
+
 // const middleware = [thunk];
-
-// const composeEnhancers =
-//   typeof window === "object" && window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_
-//     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-//     : compose;
-
-// const composingMiddlewareAndDevTools = composeEnhancers(
-//   applyMiddleware(...middleware)
-// );
 
 // const store = createStore(
 //   rootReducer,
 //   initialState,
-//   composingMiddlewareAndDevTools
+//   compose(
+//     applyMiddleware(...middleware),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
 // );
 
-// export default store;
+//export default store;
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers/index";
+
+const initialState = {};
+const middleware = [thunk];
+
+const composeEnhancers =
+  typeof window === "object" && window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const composingMiddlewareAndDevTools = composeEnhancers(
+  applyMiddleware(...middleware)
+);
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composingMiddlewareAndDevTools
+);
+
+export default store;
